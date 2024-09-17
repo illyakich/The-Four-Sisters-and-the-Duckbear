c6201c62435139a02fb7204d6cb5079495b456c1
ПК <@DESKTOP-5UNSHBK>
2024-09-17 20:08:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("how many eggs did homies collect today?");
+int eggs = Convert.ToInt32(Console.ReadLine());
+int sisters = eggs / 4;
+int duckbear = eggs % 4;
+Console.WriteLine($"The duckbear will get {duckbear} eggs, and the sisters will get {sisters} eggs each");
+// the total numbers where the sisters will get less than the duckbear are 1, 2 and 3.
